b5b646a1c1b298fcc30fe60d9adad65b68707118
smit2129 <smit2129@algonquinlive.com>
2025-07-16 19:48:52 -0400

    added Mission Selection (WIP)

--- Containment_Response/Intermediate/Build/BuildRulesProjects/Containment_ResponseModuleRules/obj/Development/Containment_ResponseModuleRules.AssemblyInfo.cs
+++ Containment_Response/Intermediate/Build/BuildRulesProjects/Containment_ResponseModuleRules/obj/Development/Containment_ResponseModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Containment_ResponseModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb443fb8d5f58374869a56f571ab2833d2a0ca05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc2dcb69c651c9c90ab40129fc7469eaef9991ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Containment_ResponseModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Containment_ResponseModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
