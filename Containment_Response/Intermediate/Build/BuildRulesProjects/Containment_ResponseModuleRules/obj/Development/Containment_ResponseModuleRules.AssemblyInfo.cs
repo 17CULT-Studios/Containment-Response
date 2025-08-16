9825d9072a82258d32e8da1906d358849e6f7e48
smit2129 <smit2129@algonquinlive.com>
2025-08-15 22:48:20 -0400

    small level generation change very WIP

--- Containment_Response/Intermediate/Build/BuildRulesProjects/Containment_ResponseModuleRules/obj/Development/Containment_ResponseModuleRules.AssemblyInfo.cs
+++ Containment_Response/Intermediate/Build/BuildRulesProjects/Containment_ResponseModuleRules/obj/Development/Containment_ResponseModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Containment_ResponseModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21114de7b671682a0d42497ca1648c955550a48c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3812202883c46fac6d50c084cacff768490574")]
 [assembly: System.Reflection.AssemblyProductAttribute("Containment_ResponseModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Containment_ResponseModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
